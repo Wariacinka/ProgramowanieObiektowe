d5bcdb4bfe409045ea133c293fcc1e3468fd9ea1
Oskar Piróg <Oskar.Pirog@microsoft.wsei.edu.pl>
2022-03-10 13:19:15 +0100

    jest git xD

--- lab01/ConsoleApp1/ConsoleApp1/Program.cs
+++ lab01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,4 +9,72 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
         }
     }
+
+
+    class Ułamek //Utwórz klasę Ułamek prywatne zmienne licznik i mianownik 
+    {
+        private int licznik;
+        private int mianownik;
+
+
+        public Ułamek() //konstruktor domyślny
+        {
+
+        }
+
+        public Ułamek(int licznik, int mianownik) //konstruktor z dwoma argumentami,
+
+        {
+
+
+        }
+
+        public Ułamek (Ułamek licznik , Ułamek mianownik) //konstruktor kopiujący (1 argument).
+
+        {
+
+
+        }
+
+        public override string ToString() //Dodaj metodę ToString().
+
+        {
+            return licznik + "+" + mianownik; //Przeładuj operatory + - * /.
+
+
+        }
+        //Zaimplementuj interfejsy IEquatable i IComparable
+        public interface icomparable
+        {
+            int compareTo(object obj);
+
+        }
+        public interface icompare<t>
+        {
+            int compareto(t other);
+
+        }
+        public int compareto(object obj)
+        {
+            return compareto((Ułamek)obj);
+
+        }
+        public int compareto(Ułamek other)
+        {
+            double d1 = (double)licznik / mianownik;
+            double d2 = (double)other.licznik / mianownik;
+            if (d1 == d2)
+                return 0;
+            return d1 < d2 ? -1 : 1; 
+        }
+
+
+        
+
+
+    }
+
+   
+   
+
 }
